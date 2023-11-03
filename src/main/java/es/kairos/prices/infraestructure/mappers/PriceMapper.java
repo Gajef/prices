c21b3751cea41b471a001590845325d2da7fe578
Gajef <jesus.garcia1650@gmail.com>
2023-11-03 13:05:33 +0100

    refactor: change protection type to public

--- src/main/java/es/kairos/prices/infraestructure/mappers/PriceMapper.java
+++ src/main/java/es/kairos/prices/infraestructure/mappers/PriceMapper.java
@@ -4,7 +4,7 @@ import es.kairos.prices.application.PriceInput;
 import es.kairos.prices.infraestructure.dto.PriceRequestDTO;
 
 public class PriceMapper {
-    private PriceInput toPriceInput(PriceRequestDTO priceRequestDTO){
+    public PriceInput toPriceInput(PriceRequestDTO priceRequestDTO){
         return new PriceInput(
                 priceRequestDTO.getStartDate(),
                 priceRequestDTO.getProductId(),
