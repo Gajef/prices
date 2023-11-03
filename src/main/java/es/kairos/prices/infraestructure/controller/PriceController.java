bade2a8d44c2ff1fd4141ec7b0e9d4ad9a89f238
Gajef <jesus.garcia1650@gmail.com>
2023-11-03 13:06:27 +0100

    chore: add mapper to controller

--- src/main/java/es/kairos/prices/infraestructure/controller/PriceController.java
+++ src/main/java/es/kairos/prices/infraestructure/controller/PriceController.java
@@ -1,8 +1,10 @@
-package es.kairos.prices.ui;
+package es.kairos.prices.infraestructure.controller;
 
-import java.time.LocalDateTime;
-
-import es.kairos.prices.application.PriceUseCase;
+import es.kairos.prices.application.GetPriceUseCase;
+import es.kairos.prices.application.PriceInput;
+import es.kairos.prices.infraestructure.dto.PriceRequestDTO;
+import es.kairos.prices.infraestructure.dto.PriceResponseDTO;
+import es.kairos.prices.infraestructure.mappers.PriceMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/prices")
 public class PriceController {
   
-  PriceUseCase priceUseCase;
+  private GetPriceUseCase getPriceUseCase;
+  private PriceMapper priceMapper;
+  
   @GetMapping("")
   ResponseEntity<PriceResponseDTO> getPrice(@RequestBody PriceRequestDTO priceRequestDTO) {
-    return ResponseEntity.ok().body(priceUseCase.getPrice(priceRequestDTO));
+    PriceInput priceInput = priceMapper.toPriceInput(priceRequestDTO);
+    return ResponseEntity.ok().body(getPriceUseCase.getPrice(priceInput));
   }
 }
