e6a2e514a2050f32a5107c92bca51121907ecf9b
Gajef <jesus.garcia1650@gmail.com>
2023-11-03 13:32:21 +0100

    chore: add mapper ffrom output to dto

--- src/main/java/es/kairos/prices/infrastructure/mappers/PriceInfrastructureMapper.java
+++ src/main/java/es/kairos/prices/infrastructure/mappers/PriceInfrastructureMapper.java
@@ -1,7 +1,9 @@
 package es.kairos.prices.infrastructure.mappers;
 
 import es.kairos.prices.application.PriceInput;
+import es.kairos.prices.application.PriceOutput;
 import es.kairos.prices.infrastructure.dto.PriceRequestDTO;
+import es.kairos.prices.infrastructure.dto.PriceResponseDTO;
 
 public class PriceInfrastructureMapper {
     public PriceInput toPriceInput(PriceRequestDTO priceRequestDTO){
@@ -9,6 +11,17 @@ public class PriceInfrastructureMapper {
                 priceRequestDTO.getStartDate(),
                 priceRequestDTO.getProductId(),
                 priceRequestDTO.getBrandId()
+        );
+    }
+
+    public PriceResponseDTO toPriceResponseDTO(PriceOutput priceOutput){
+        return new PriceResponseDTO(
+                priceOutput.getProductId(),
+                priceOutput.getBrandId(),
+                priceOutput.getPriceList(),
+                priceOutput.getStartDate(),
+                priceOutput.getEndDate(),
+                priceOutput.getPrice()
         );
     }
 }
