b5e4ae198e01394f47bff6f6ed1ecc6b9c98a533
Gajef <jesus.garcia1650@gmail.com>
2023-11-03 13:15:10 +0100

    chore: implement mapper for output to dto

--- src/main/java/es/kairos/prices/application/PriceApplicationMapper.java
+++ src/main/java/es/kairos/prices/application/PriceApplicationMapper.java
@@ -1,4 +1,16 @@
 package es.kairos.prices.application;
 
+import es.kairos.prices.infrastructure.dto.PriceResponseDTO;
+
 public class PriceApplicationMapper {
+    public PriceResponseDTO toPriceResponseDTO(PriceOutput priceOutput){
+        return new PriceResponseDTO(
+                priceOutput.getProductId(),
+                priceOutput.getBrandId(),
+                priceOutput.getPriceList(), 
+                priceOutput.getStartDate(),
+                priceOutput.getEndDate(),
+                priceOutput.getPrice()
+        );
+    }
 }
